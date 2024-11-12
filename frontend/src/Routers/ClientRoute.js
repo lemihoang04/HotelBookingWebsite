ff9f6adeeb3f343910cb221ff8d72b9a191d717c
Nguyen Quang Minh <quangminh23122004@gmail.com>
2024-11-12 17:03:22 +0700

    Add component

---
+++ frontend/src/Routers/ClientRoute.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Home from "../pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const ClientRoute = () => {
+	return (
+		<div>
+			<Switch>
+				{/* <PrivateRoutes path="/users" component={Users} />
+				<PrivateRoutes path="/ticket" exact component={Ticket} />
+				<PrivateRoutes path="/info-car/id=:id_user" component={InfoCar} />
+				<PrivateRoutes path="/ticket/create" component={Add_Ticket} />
+				<PrivateRoutes path="/ProcessPayment" component={PaymentCall} />
+				<PrivateRoutes path="/SlotCar" component={Slot_Car} />
+				<PrivateRoutes path="/Account/DepositMoney" component={DepositMoney} /> */}
+				{/* <Route path="/admin">
+					<Login_Admin />
+				</Route> */}
+				{/* <Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/users">users</Route> */}
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="*">404 Not Found</Route>
+			</Switch>
+		</div>
+	);
+};
+
+export default ClientRoute;
